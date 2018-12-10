49f395a181bbb890604c13e61f8d816e2ac87b87
Jordi <32875964+Jordifusbel@users.noreply.github.com>
2018-12-10 18:32:20 +0100

    Añadido Doofous

---
+++ Doofous.js
@@ -0,0 +1,8 @@
+function Doofous() {
+    this.monedas= ["R2-D2","R2-D2","R2-D2","R2-D2"];
+    this.speak= function{
+    	return "Tengo una colección de monedas antiguas raras!";
+    };
+    this.id= "J-19-Z7";
+    this.ondas= "altas";
+}
